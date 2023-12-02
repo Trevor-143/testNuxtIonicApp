9a86ff29fbd5c7ab4433264bd8ba924af3c218a3
Trevor_143 <trevorarapu2@gmail.com>
2023-12-02 14:21:44 +0300

    more tests

--- pages/index.vue
+++ pages/index.vue
@@ -1,26 +1,24 @@
 <template>
     <ion-page>
-        <ion-header :translucent="true" >
+        <!-- <ion-header :translucent="true" >
             <ion-toolbar>
                 <ion-title>Home page</ion-title>
             </ion-toolbar>
-        </ion-header>
+        </ion-header> -->
 
         <ion-content :fullscreen="true" >
-            <ion-header collapse="condense" class="topHeadTool" >
+            <!-- <ion-header collapse="condense" class="topHeadTool" >
                 <ion-toolbar >
                     <ion-title>Welcome page</ion-title>
                 </ion-toolbar>
-            </ion-header>
+            </ion-header> -->
             <div class="content">
                 <h1>Caalee</h1>
                 <p>Icon & logo wallpapers</p>
-                <ion-img src="/images/welcomeImage.png" alt="caalee welcome image" ></ion-img>
-                <ion-button 
-                    class="custom-button"
-                    style="--border-radius: 2rem; margin-top: 3rem;" 
-                    router-link="/tabs/tab1" 
-                >Get Started</ion-button>
+                <ion-img src="/images/lightWelcomeGif.gif" alt="caalee welcome image" ></ion-img>
+                <button class="custom-button" >
+                    <ion-nav-link router-link="/tabs/tab1" >Get Started</ion-nav-link>
+                </button>
             </div>
         </ion-content>
     </ion-page>
@@ -36,13 +34,25 @@
     align-items: center;
     justify-content: center;
     flex-direction: column;
-    height: 80%;
+    height: 100%;
 }
 .content h1 {
     font-size: 4rem;
     font-weight: 800;
 }
-.custom-button {
-    padding: 0;
+.content p {
+    margin-bottom: 2rem;
+    margin-top: -1rem;
 }
+.custom-button {
+    padding: 1.5rem 1rem;
+    width: 70%;
+    font-size: 1.5rem;
+    margin-top: 3rem;
+    background-color: var(--primary);
+    color: #ffffff;
+    border-radius: 99px;
+    font-weight: 800;
+}
+
 </style>
